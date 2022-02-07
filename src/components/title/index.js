c099330b3a1ca012e8112e3fd406906a3f5838c0
Damon <damondias@gmail.com>
2022-02-07 15:08:19 -0400

    Tela da carteira(home) criada

---
+++ src/components/title/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const TitleStyled = styled.h1`
+font-style: normal;
+font-weight: bold;
+font-size: 26px;
+line-height: 31px;
+color: #FFFFFF;
+`;
+
+export default TitleStyled;
