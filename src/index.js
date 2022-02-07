15459ca5df8b8e64865993d1d7793c6fcfc6a634
Damon <damondias@gmail.com>
2022-02-07 12:48:12 -0400

    Configs iniciais e instalçao de bibliotecas do projeto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/reset.css'
+import './style/style.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
